deb9210e80ab441aef1edc08b1949aa8c6a2eb76
Shanmugam_M <Shanmugam_M@dell.com>
2018-11-19 20:18:05 -0600

    Module is completed

---
+++ StarChart/Controllers/CelestrialController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestrialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CelestrialObjectController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        // GET
+       
+    }
+}
